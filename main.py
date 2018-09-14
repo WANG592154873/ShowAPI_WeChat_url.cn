ba18e8b2c053a1f3d00e0138f8fbffb97545aa00
了不起的菠萝君 <592154873@qq.com>
2018-09-14 16:04:37 +0800

    增加批量调用功能，增加延时

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
+# python3.6.5
+# 需要引入requests包 ：运行终端->进入python/Scripts ->输入：pip install requests
 from ShowapiRequest import ShowapiRequest
-
-r = ShowapiRequest("http://route.showapi.com/1311-1","my_appId","my_appSecret" )
-r.addBodyPara("long", "http://www.baidu.com")
-res = r.post()
-
-f = open("test.txt",'a')
-f.write(res.text) 
-print(res.text) # 返回信息
+x = input("请输入需要缩短的网址：（例如：www.baidu.com）\n")
+num = int(input("请输入要生成的数量：\n"))
+import time
+def sleep(mytime=""):
+        time.sleep(mytime)
+while num > 0:
+    f = open("test.txt", 'a')
+    r = ShowapiRequest("http://route.showapi.com/1311-1","72683","028b760013994ee9ab2cc01601603fbb" )
+    r.addBodyPara("long", x)
+    res = r.post()
+    #print(res.text) # 返回信息
+    f.write(res.text)
+    f.write("\r\n")
+    print(res.text)
+    sleep(2)
+    num = num - 1
+print("调用完成，请按任意键退出！")
+end1 = input()
